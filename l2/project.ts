388db9d23ef75f12c6d5b8cc25fcab894c1f2a01
Lucas Mafra <36516904+lucasexpansiva@users.noreply.github.com>
2026-02-20 14:54:20 -0300

    group-1
    group-1

---
+++ l2/project.ts
@@ -0,0 +1,15 @@
+/// <mls fileReference="_102031_/l2/project.ts" enhancement="_blank" />
+
+export const projectConfig = {
+    masterFrontEnd: {
+        build: '',
+        start: '',
+        liveView: '',
+    },
+    masterBackEnd: {
+        build: '',
+        start: '',
+        serverView: ''
+    },
+    modules: []
+}
